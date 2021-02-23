9df7afe1063410647535d9f44fcf03748bd0ffe1
pf <paul@falstad.com>
2021-02-23 13:57:54 -0800

    show charge on edges of walls

--- src/com/falstad/emstatic/client/Wall.java
+++ src/com/falstad/emstatic/client/Wall.java
@@ -43,14 +43,79 @@ public class Wall extends DragObject {
 		setTransform();
 		pot = Double.parseDouble(st.nextToken());
 	}
+
+	/*
+	    void drawMaterials(boolean residual) {
+		EMStatic.drawMedium(topLeft.x, topLeft.y, topRight.x, topRight.y, bottomLeft.x, bottomLeft.y, bottomRight.x,
+			bottomRight.y, 0, residual ? 0 : pot);
+	    }
+
+	    void draw() {
+		super.draw();
+		EMStatic.displayBoxCharge(topLeft.x, topLeft.y, topRight.x, topRight.y, bottomLeft.x, bottomLeft.y, bottomRight.x, bottomRight.y);
+	    }
+	    */
+	   
+	double wallLen;
 	
+	void setWallTransform() {
+	    wallLen = Math.hypot(handles.get(0).x-handles.get(1).x, handles.get(0).y-handles.get(1).y);
+	    double cx = (handles.get(0).x+handles.get(1).x)/2;
+	    double cy = (handles.get(0).y+handles.get(1).y)/2;
+	    /*
+	    double xf[] = new double[6];
+	    xf[0] = xf[4] = (handles.get(0).y-cy)*2/wallLen;
+	    xf[1] = (handles.get(0).x-cx)*2/wallLen;
+            xf[3] = -xf[1];
+//            xf[2] = (1-xf[0])*cx - xf[1]*cy;
+//            xf[5] = -xf[3]*cx + (1-xf[4])*cy;
+            xf[2] = cx;
+            xf[5] = cy;
+            EMStatic.setTransform(xf[0], xf[1], xf[2], xf[3], xf[4], xf[5]);
+            */
+	    double xf0 = (handles.get(0).y-cy)*2/wallLen;
+	    double xf1 = (handles.get(0).x-cx)*2/wallLen;
+	    EMStatic.setTransform(xf0, xf1, cx, -xf1, xf0, cy);
+	}
+
+	void drawMaterials(boolean residual) {
+	    setWallTransform();
+	    /*
+	    int len = (int)Math.hypot(handles.get(0).x-handles.get(1).x, handles.get(0).y-handles.get(1).y);
+	    int cx = (int) ((handles.get(0).x+handles.get(1).x)/2);
+	    int cy = (int) ((handles.get(0).y+handles.get(1).y)/2);
+	    cx = cy = 0;
+	    EMStatic.drawMedium(cx-2, cy-len/2, cx+2, cy-len/2, cx-2, cy+len/2, cx+2, cy+len/2, 0, residual ? 0 : pot);
+	    */
+	    int len2 = (int) (wallLen/2);
+	    EMStatic.drawMedium(-2, -len2, 2, -len2, -2, len2, 2, len2, 0, residual ? 0 : pot); 
+	}
+	
+	void draw() {
+	    super.draw();
+	    setWallTransform();
+	    /*
+	    int len = (int)Math.hypot(handles.get(0).x-handles.get(1).x, handles.get(0).y-handles.get(1).y);
+	    int cx = (int) ((handles.get(0).x+handles.get(1).x)/2);
+	    int cy = (int) ((handles.get(0).y+handles.get(1).y)/2);
+	    cx = cy = 0;
+	    EMStatic.displayBoxCharge(cx-2, cy-len/2, cx+2, cy-len/2, cx-2, cy+len/2, cx+2, cy+len/2);
+	    */
+	    int len2 = (int) (wallLen/2);
+	    EMStatic.displayBoxCharge(-2, -len2, 2, -len2, -2, len2, 2, len2);
+	}
+	
+	/*
 	void drawSelection() {
 	    drawMaterials(false);
 	}
+	*/
 	
+	/*
 	void drawMaterials(boolean residual) {
 		EMStatic.drawWall(handles.get(0).x, handles.get(0).y, handles.get(1).x, handles.get(1).y, residual ? 0 : pot);
 	}
+	*/
 	
 	int getDumpType() { return 'w'; }
 
